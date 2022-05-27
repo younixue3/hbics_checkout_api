1d8af69e947a7d4cd48df585d6ab10599521035f
younixue3 <bussiness.needs.rctka@gmail.com>
2022-05-27 17:11:05 +0800

    Fixing

--- approvalCard/models.py
+++ approvalCard/models.py
@@ -34,7 +34,6 @@ class permissions(models.Model):
 
 class cards(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-<<<<<<< HEAD
     permission_uuid = models.ManyToManyField(permissions)
 
 class authUserCard(models.Model):
@@ -43,34 +42,26 @@ class authUserCard(models.Model):
         'cards',
         on_delete=models.DO_NOTHING,
     )
-=======
     permission_uuid = models.ManyToManyField(permissions, blank=True, null=True)
->>>>>>> 46d1768ff0598756f664c4a0b6c67075df11f79b
     user_uuid = models.ForeignKey(
         User,
         on_delete=models.DO_NOTHING
     )
 
-<<<<<<< HEAD
 class authUserDivision(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-=======
 class profile_pic(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     namefile = models.TextField()
->>>>>>> 46d1768ff0598756f664c4a0b6c67075df11f79b
     user_uuid = models.ForeignKey(
         User,
         on_delete=models.DO_NOTHING
     )
     created_at = models.DateTimeField(auto_now_add=True)
-<<<<<<< HEAD
     updated_at = models.DateTimeField(auto_now=True)
     deleted_at = models.DateTimeField(blank=True, null=True)
 
 class authDivision(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=15)
-=======
     deleted_at = models.DateTimeField(blank=True, null=True)
->>>>>>> 46d1768ff0598756f664c4a0b6c67075df11f79b
