6a1d81e46f540744b0e4813cbd75956ec8b5023b
Ricko Tiaka <78606675+younixue3@users.noreply.github.com>
2022-05-31 11:00:27 +0800

    Update urls.py

--- quickstart/urls.py
+++ quickstart/urls.py
@@ -18,9 +18,9 @@ router.register(r'permission', PermissionViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('auth', CustomAuthToken.as_view()),
-    path('cards/', card_list)
+    path('cards/', card_list),
     path('approvals/', include('approvalCard.urls'))
-
+]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     # Note Install Pillow pip install pillow
